68a9b6a02aba03513ec485d0ae1e3af95faf3a92
Dima Lavrishyn <dima.lavrishin@gmail.com>
2019-11-13 10:28:52 +0200

    Lab5 - optimization of algorithm

--- wchain.py
+++ wchain.py
@@ -6,7 +6,7 @@ dynamic = dict()
 result = 1
 with open("wchain.in", "r") as f:
     n = int(f.readline())
-    words = [[] for i in range(51)]
+    words = [[] for i in range(55)]
 
     for i in range(n):
         s = f.readline()
@@ -14,16 +14,21 @@ with open("wchain.in", "r") as f:
         min_len = min(min_len, len(s))
         max_len = max(max_len, len(s))
 
-for word in words[min_len]:
-    dynamic[word] = 1
+for i in range(min_len, max_len + 1):
+    if len(words[i-1]) == 0:
+        for word in words[i]:
+            dynamic[word] = 1
+        continue
 
-for i in range(min_len + 1, max_len + 1):
     for word in words[i]:
         curr_res = 1
         for k in range(i + 1):
             new_word = word[:k] + word[k+1:]
             if new_word in dynamic:
                 curr_res = max(curr_res, dynamic[new_word] + 1)
+
+            if (curr_res == i - min_len + 1):
+                break
 
         dynamic[word] = curr_res
         result = max(result, curr_res)
